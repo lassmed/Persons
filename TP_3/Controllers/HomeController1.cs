a5d82c472e3efe172650e66490771d3ab26730f9
USER <USER@DESKTOP-E8JOF5O>
2022-12-11 19:10:22 +0100

    Ajoutez des fichiers projet.

---
+++ TP_3/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_3.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
